c397d4c200726d2590a7ebbb8ce6ff5c9b120791
dengyaolong <dengyaolong@yeah.net>
2017-09-03 23:00:03 +0800

    shame..

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const debug = require('debug')('koa-sso-auth')
 
-module.exports =  async (ctx, next) => {
+/**
+ * sso auth middleware.
+ *
+ * @param {Object} options
+ *  - {Function(url)} loginURL function formatter, set the login url.
+ */
 
+module.exports =  function(options) {
+    return async (ctx, next) => {
+        let loginURL = options.loginURL(ctx.path)
+
+    }
 }
-
